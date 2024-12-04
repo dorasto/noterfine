9f1e58b169889b53fb820493f0193e4c4f364733
Tommy Lundy <tommerty@doras.to>
2024-12-04 16:54:26 +0000

    admin

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -2,11 +2,13 @@ import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "@/app/db"; // your drizzle instance
 import * as schema from "@/app/db/schema/auth-schema";
+import { admin } from "better-auth/plugins";
 
 export const auth = betterAuth({
     emailAndPassword: {
         enabled: true,
     },
+    plugins: [admin()],
     database: drizzleAdapter(db, {
         provider: "pg",
         schema,
